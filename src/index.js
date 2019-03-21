07cb43a87fb327267b946a382be4d46c10129c1a
Prashanth <yvprashanth@gmail.com>
2019-03-20 23:45:06 -0700

    redux changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+console.log(store.getState())
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
