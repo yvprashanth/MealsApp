ac8ec409d2e3e2030f6fb52b17a543da5a1d63e6
Prashanth <yvprashanth@gmail.com>
2019-03-20 23:56:37 -0700

    Redux action submitted

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window
 
 console.log(store.getState())
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
